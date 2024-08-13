3811722448637b2951bd1720fbf7b14cbf23eed6
Pratham <rimo.coder@gmail.com>
2024-08-13 12:08:52 +0530

    Re_RoutingDP

--- Re_Routing_TreeDP.cpp
+++ Re_Routing_TreeDP.cpp
@@ -2,8 +2,6 @@
 // https://codeforces.com/contest/1092/problem/F
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
 #define vii vector<pair<int,int>>
 #define vvi vector<vector<int>>
@@ -17,72 +15,6 @@
 #define rep(i,a,n) for(int i=a;i<n;i++)
 #define Faster ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 using namespace std;
-
-using namespace __gnu_pbds;
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> T_set; // PBDS_set
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> T_multiset; // PBDS_multiset
-void myerase(T_multiset &t, int v){
-    int rank = t.order_of_key(v);//Number of elements that are less than v in t
-    T_multiset::iterator it = t.find_by_order(rank); //Iterator that points to the (rank+1)th element in t
-    t.erase(it);
-}
-int gcd(int a,int b) {
-    if(b==0) return a;
-    else return gcd(b,a%b);
-}
-int lcm(int &a, int &b) { return a * b / __gcd(a, b); }
-
-const int MAX = (int) 1000;
-int spf[MAX];
-bool vis[MAX];
-const ll inf=1e18;
-
-void spf_Sieve(){
-    spf[1]=1;
-    memset(vis,false,MAX);
-    for(int i=2;i<MAX;i+=2) {
-        vis[i]=true;
-        spf[i]=2;
-    }
-
-    for(ll i=3;i<MAX;i+=2){
-        if(!vis[i]){
-            spf[i]=i;
-            for(ll j=i; (i*j)<MAX; j+=2){
-                if(!vis[i*j]){
-                    vis[i*j]=true;
-                    spf[i*j] = i;
-                }
-            }
-        }
-    }
-}
-
-vector<int> prime_factors(int dvd){
-
-    vector<int> pf;
-    while(dvd!=1){
-        int x = spf[dvd], c=0;
-        while(spf[dvd]==x){
-            c++;
-            dvd = dvd/x;
-        }
-        pf.push_back(c);
-    }
-    return pf;
-}
-
-long long binexp(long long a, long long b, long long m) {
-    a %= m;
-    long long res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
 
 const ll N = 2e5;
 vector<ll> adj[N +1];
